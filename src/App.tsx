14fda2b241650be26ee6dc7f94c25d7d4b200d38
Almazov Urmat <almazov.urmat@gmail.com>
2023-05-30 15:03:29 +0600

    added burger's design and style

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import Burger from "./components/Burger/Burger";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const styles: React.CSSProperties = {
+        width: "1400px",
+        display: "flex",
+        justifyContent: "space-between",
+    };
+
+    return (
+        <div style={styles}>
+            <div className="side ingredient-side">
+                <h3 className="blockTitle">Ingredient</h3>
+            </div>
+            <div className="side burder-side">
+                <h3 className="blockTitle">Burger</h3>
+                <Burger />
+            </div>
+        </div>
+    );
 }
 
 export default App;
